a3e6070a941e0250772affced5938297b8905142
Offliners <eric21904@gmail.com>
2025-04-03 22:14:05 +0800

    prevent infinite loop2

--- student_agent.py
+++ student_agent.py
@@ -259,7 +259,8 @@ class Game2048Env(gym.Env):
         # If the simulated board is different from the current board, the move is legal
         return not np.array_equal(self.board, temp_board)
 file_id = "1MWt7BP5-4dXnjDu5iq9edZGLAQr1i-BO"
-url=f"https://drive.google.com/uc?id={file_id}"gdown.download(url , "stage_1.pkl",quiet=False, fuzzy=True)
+url=f"https://drive.google.com/uc?id={file_id}"
+gdown.download(url , "stage_1.pkl",quiet=False, fuzzy=True)
 patterns = [[(0,0),(1,0),(2,0),(3,0),(2,1),(3,1)],[(0,1),(1,1),(2,1),(3,1),(2,2),(3,2)],[(0,1),(1,1),(2,1),(0,2),(1,2),(2,2)],[(0,2),(1,2),(2,2),(0,3),(1,3),(2,3)]]
 approximator_1=NTupleApproximator(board_size=4, patterns=patterns)
 with open('stage_1.pkl', 'rb') as f:
