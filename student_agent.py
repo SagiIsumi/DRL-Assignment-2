0a8c380d3954f67df34f085b499270551ac9d4fc
Offliners <eric21904@gmail.com>
2025-04-03 15:54:09 +0800

    new numpy version pkl

--- student_agent.py
+++ student_agent.py
@@ -263,7 +263,7 @@ def get_action(state, score):
     #return random.choice([0, 1, 2, 3]) # Choose a random action
     
     # You can submit this random agent to evaluate the performance of a purely random strategy.
-    file_id = "1NgVor7szhiXwZf0z1lDXgilC0_O3RCES"
+    file_id = "17IiAVLIdtPYFK-2QKAQ5DZAYJPe1YeL9"
     gdown.download(f"https://drive.google.com/uc?id={file_id}", "stage_1.pkl", quiet=False)
     patterns = [[(0,0),(1,0),(2,0),(3,0),(2,1),(3,1)],[(0,1),(1,1),(2,1),(3,1),(2,2),(3,2)],[(0,1),(1,1),(2,1),(0,2),(1,2),(2,2)],[(0,2),(1,2),(2,2),(0,3),(1,3),(2,3)]]
     approximator_1=NTupleApproximator(board_size=4, patterns=patterns)
